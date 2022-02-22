5284f0e65ecacab94ed6b7445fa8c56eaa3baa29
Factoria F5 <andresp199519@gmail.com>
2022-02-23 00:07:57 +0100

    Se modificaron los componentes para que tuviera la funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="App">
-<h1>Hola, mundo</h1>
+    <Container />
     </div>
   );
 }
